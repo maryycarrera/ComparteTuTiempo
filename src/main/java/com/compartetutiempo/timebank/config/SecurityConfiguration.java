a3e830b95df2ef36fcbdccad457f4279080602b1
María Carrera <marcartal1@alum.us.es>
2025-08-16 19:25:45 +0200

    feat: app and system config

--- src/main/java/com/compartetutiempo/timebank/config/SecurityConfiguration.java
+++ src/main/java/com/compartetutiempo/timebank/config/SecurityConfiguration.java
@@ -2,14 +2,24 @@ package com.compartetutiempo.timebank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.compartetutiempo.timebank.jwt.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,7 +30,11 @@ public class SecurityConfiguration {
                     .requestMatchers("/auth/**").permitAll()
                     .anyRequest().authenticated()
                 )
-            .formLogin(withDefaults())
+            .sessionManagement(sessionManager -> sessionManager
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+            .authenticationProvider(authProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
     
