7629207fba31de77a748091946b6c97fbf1c5c19
María Carrera <marcartal1@alum.us.es>
2025-08-16 13:11:32 +0200

    feat: add dashboard component

---
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../shared/navbar/navbar';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [Navbar],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard {
+
+  userLoginOn:boolean = false;
+
+}
