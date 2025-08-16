2004eafca7e62dd4c8ef7e0179c11d4668364e5f
María Carrera <marcartal1@alum.us.es>
2025-08-16 13:11:14 +0200

    feat: add login component

---
+++ frontend/src/app/auth/login/login.ts
@@ -0,0 +1,64 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/auth/login.service';
+import { LoginRequest } from '../../services/auth/login-request';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private loginService = inject(LoginService);
+  private http = inject(HttpClient);
+
+  loginForm;
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      username: ['admin1', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9_.]+$')
+      ]],
+      password: ['', [
+        Validators.required,
+      ]]
+    });
+  }
+
+  get username() {
+    return this.loginForm.controls.username;
+  }
+
+  get password() {
+    return this.loginForm.controls.password;
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (userData) => {
+          console.log(userData);
+        },
+        error: (errorData) => {
+          console.error(errorData);
+        },
+        complete: () => {
+          console.info('Login request completed')
+        }
+      })
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
+    } else {
+      this.loginForm.markAllAsTouched();
+      alert('Credenciales no válidas')
+    }
+  }
+
+}
