203b8b30b26a854f7d0003b3bb445bf37572e419
María Carrera <marcartal1@alum.us.es>
2025-08-16 19:22:58 +0200

    fix: add required args constructor annotation

--- src/main/java/com/compartetutiempo/timebank/demo/DemoController.java
+++ src/main/java/com/compartetutiempo/timebank/demo/DemoController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class DemoController {
 
     @PostMapping("/demo")
