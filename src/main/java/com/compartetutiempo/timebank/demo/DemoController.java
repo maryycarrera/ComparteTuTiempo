3c5852a186dddb372da867ad4867c1660675454c
María Carrera <marcartal1@alum.us.es>
2025-08-16 18:13:27 +0200

    feat: basic routes

---
+++ src/main/java/com/compartetutiempo/timebank/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.compartetutiempo.timebank.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+
+    @PostMapping("/demo")
+    public String welcome() {
+        return "Welcome from secure endpoint";
+    }
+
+}
