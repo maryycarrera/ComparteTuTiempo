fe52c9109edfdedf5193a94f5ffd41646e926101
María Carrera <marcartal1@alum.us.es>
2025-08-16 13:13:45 +0200

    feat: config app routing and content

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inicio', component: Dashboard },
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'iniciar-sesion', component: Login },
+    { path: '**', redirectTo: '/inicio' }
+];
