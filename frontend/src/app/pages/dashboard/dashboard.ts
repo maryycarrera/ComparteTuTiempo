24854a36279aa328fd410000c28ba4e48791c492
María Carrera <marcartal1@alum.us.es>
2025-08-16 14:13:46 +0200

    feat: behavior subject to handle login
    Communication between components

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Navbar } from '../../shared/navbar/navbar';
+import { LoginService } from '../../services/auth/login.service';
+import { User } from '../../services/auth/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,8 +10,31 @@ import { Navbar } from '../../shared/navbar/navbar';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit, OnDestroy {
 
   userLoginOn: boolean = false;
+  userData?: User;
 
+  private loginService = inject(LoginService);
+  private userLoginSubscription: Subscription = new Subscription();
+  private userDataSubscription: Subscription = new Subscription();
+
+  ngOnInit(): void {
+    this.userLoginSubscription.add(this.loginService.currentUserLoginOn.subscribe({
+      next: (userLoginOn) => {
+        this.userLoginOn = userLoginOn;
+      }
+    }));
+
+    this.userDataSubscription.add(this.loginService.userData.subscribe({
+      next: (userData) => {
+        this.userData = userData;
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.userLoginSubscription.unsubscribe();
+    this.userDataSubscription.unsubscribe();
+  }
 }
--- frontend/src/app/services/auth/login.service.ts
+++ frontend/src/app/services/auth/login.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { LoginRequest } from './login-request';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, throwError, BehaviorSubject, tap } from 'rxjs';
 import { User } from './user';
 
 @Injectable({
@@ -11,10 +11,17 @@ export class LoginService {
 
   private http = inject(HttpClient);
 
+  currentUserLoginOn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  currentUserData: BehaviorSubject<User> = new BehaviorSubject<User>({ id: 0, username: '' });
+
   constructor() { }
 
-  login(credentials:LoginRequest):Observable<User> {
+  login(credentials: LoginRequest): Observable<User> {
     return this.http.get<User>('/assets/data.json').pipe(
+      tap((userData: User) => {
+        this.currentUserData.next(userData);
+        this.currentUserLoginOn.next(true);
+      }),
       catchError(this.handleError)
     );
   }
@@ -26,6 +33,14 @@ export class LoginService {
       console.error('Backend returned code:', error.status, 'body was:', error.error);
     }
     return throwError(() => new Error('Something went wrong; please try again.'));
+  }
+
+  get userData(): Observable<User> {
+    return this.currentUserData.asObservable();
+  }
+
+  get userLoginOn(): Observable<boolean> {
+    return this.currentUserLoginOn.asObservable();
   }
 
 }
