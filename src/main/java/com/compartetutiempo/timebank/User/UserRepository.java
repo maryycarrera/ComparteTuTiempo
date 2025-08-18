94b228f1d23fa603caf6e165b15fb0e2db07de83
María Carrera <marcartal1@alum.us.es>
2025-08-18 18:46:04 +0200

    fix: remove unused imports

--- src/main/java/com/compartetutiempo/timebank/User/UserRepository.java
+++ src/main/java/com/compartetutiempo/timebank/User/UserRepository.java
@@ -3,7 +3,6 @@ package com.compartetutiempo.timebank.User;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
