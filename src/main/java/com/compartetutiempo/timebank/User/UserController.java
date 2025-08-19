40dd20816762a9bbe73a35753ea1a35051de6282
María Carrera <marcartal1@alum.us.es>
2025-08-19 21:07:09 +0200

    fix: fix api endpoint for users

--- src/main/java/com/compartetutiempo/timebank/User/UserController.java
+++ src/main/java/com/compartetutiempo/timebank/User/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 @CrossOrigin(origins = {"http://localhost:4200"}) // TODO: Modificar posteriormente para desplegar en la nube
 public class UserController {
