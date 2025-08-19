1c938438a584ec75cdaed712375ee6f9ca27f1c7
María Carrera <marcartal1@alum.us.es>
2025-08-19 19:14:43 +0200

    feat: personal-details module added to frontend

---
+++ frontend/src/app/components/personal-details/personal-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-details',
+  imports: [],
+  templateUrl: './personal-details.html',
+  styleUrl: './personal-details.css'
+})
+export class PersonalDetails {
+
+}
