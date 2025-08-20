1a5e1236e390cd031742113997c6f9dd4191e33e
María Carrera <marcartal1@alum.us.es>
2025-08-20 20:06:02 +0200

    fix: relocate exceptions package

--- src/main/java/com/compartetutiempo/timebank/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/compartetutiempo/timebank/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.compartetutiempo.exceptions;
+package com.compartetutiempo.timebank.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
