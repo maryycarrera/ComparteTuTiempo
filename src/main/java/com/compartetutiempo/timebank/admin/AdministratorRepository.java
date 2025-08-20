998e749ec2d1f5c33d5820c807c10e057a9451d1
María Carrera <marcartal1@alum.us.es>
2025-08-20 20:22:32 +0200

    feat: add admin repository, service, controller

---
+++ src/main/java/com/compartetutiempo/timebank/admin/AdministratorRepository.java
@@ -0,0 +1,16 @@
+package com.compartetutiempo.timebank.admin;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdministratorRepository extends CrudRepository<Administrator, Integer> {
+
+    @Query("SELECT admin FROM Administrator admin WHERE admin.user.username = :username")
+    Optional<Administrator> findAdministratorByUser(String username);
+
+    @Query("SELECT admin FROM Administrator admin WHERE admin.user.id = :userId")
+    Optional<Administrator> findAdministratorByUser(int userId);
+
+}
---
+++ src/main/java/com/compartetutiempo/timebank/admin/AdministratorRestController.java
@@ -0,0 +1,35 @@
+package com.compartetutiempo.timebank.admin;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admins")
+public class AdministratorRestController {
+
+    private final AdministratorService administratorService;
+
+    @Autowired
+    public AdministratorRestController(AdministratorService administratorService) {
+        this.administratorService = administratorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Administrator>> findAllAdministrators() {
+        List<Administrator> administrators = (List<Administrator>) administratorService.findAll();
+        return ResponseEntity.ok(administrators);
+    }
+
+    @GetMapping(value = "{adminId}")
+    public ResponseEntity<Administrator> findById(@PathVariable("adminId") Integer adminId) {
+        Administrator administrator = administratorService.findAdministrator(adminId);
+        return ResponseEntity.ok(administrator);
+    }
+
+}
