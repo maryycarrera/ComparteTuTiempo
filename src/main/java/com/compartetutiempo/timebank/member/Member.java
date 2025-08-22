f90170398f8312953a3058445edeade2bf36eeac
María Carrera <marcartal1@alum.us.es>
2025-08-22 19:11:34 +0200

    feat: add member logic to backend

---
+++ src/main/java/com/compartetutiempo/timebank/member/Member.java
@@ -0,0 +1,57 @@
+package com.compartetutiempo.timebank.member;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.compartetutiempo.timebank.model.Person;
+import com.compartetutiempo.timebank.user.User;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "members")
+public class Member extends Person {
+
+    @Column(name = "date_of_membership")
+    @NotNull
+    private LocalDate dateOfMembership;
+
+    @Size(max = 500)
+    private String biography;
+
+    @NotNull
+    @Min(-5)
+    @Max(10)
+    private Integer hours;
+
+    @NotNull
+    @Min(0)
+    @Max(59)
+    private Integer minutes;
+
+    @OneToOne(cascade = { CascadeType.DETACH, CascadeType.REFRESH, CascadeType.PERSIST })
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    // TODO: Debe estar entre -5 y 10
+    public Double getTimeBalance() {
+        return hours + minutes / 60.0;
+    }
+
+}
---
+++ src/main/java/com/compartetutiempo/timebank/member/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.compartetutiempo.timebank.member;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRepository extends CrudRepository<Member, Integer> {
+
+    @Query("SELECT m FROM Member m WHERE m.user.username = :username")
+    Optional<Member> findMemberByUser(String username);
+
+    @Query("SELECT m FROM Member m WHERE m.user.id = :userId")
+    Optional<Member> findMemberByUser(int userId);
+
+}
