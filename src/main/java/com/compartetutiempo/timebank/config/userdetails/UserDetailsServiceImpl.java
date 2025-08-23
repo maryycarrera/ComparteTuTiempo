b4db808c3cefba4036cabec284dc3a8e7e1ea773
María Carrera <marcartal1@alum.us.es>
2025-08-23 18:40:51 +0200

    feat: implement userdetails

---
+++ src/main/java/com/compartetutiempo/timebank/config/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.compartetutiempo.timebank.config.userdetails;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.compartetutiempo.timebank.user.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserDetailsImpl::build)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+    
+}
