529628fc0e2c88991ddc28f8b372bcfe8c9644d1
María Carrera <marcartal1@alum.us.es>
2025-08-26 17:57:29 +0200

    feat: add home component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard';
 import { Login } from './auth/login/login';
 
 import { CanActivateFn } from '@angular/router';
+import { Home } from './shared/home/home';
 
-// Guard temporal para decidir si mostrar login o dashboard
+// Guard temporal para decidir si mostrar login o home
 const inicioGuard: CanActivateFn = (route, state) => {
     // Aquí deberías inyectar el servicio de login y devolver true/false según el estado
     // Por ahora, siempre redirige al login
@@ -12,7 +12,7 @@ const inicioGuard: CanActivateFn = (route, state) => {
 };
 
 export const routes: Routes = [
-    { path: 'inicio', component: Dashboard },
+    { path: 'inicio', component: Home },
     { path: '', component: Login, canActivate: [inicioGuard] },
     { path: 'iniciar-sesion', component: Login },
     { path: '**', redirectTo: '/inicio' }
