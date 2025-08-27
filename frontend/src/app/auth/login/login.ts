64f75e5da0214e4dee1db84156dc38f00f1a5182
María Carrera <marcartal1@alum.us.es>
2025-08-27 17:24:47 +0200

    fix: translate error messages

--- frontend/src/app/auth/login/login.ts
+++ frontend/src/app/auth/login/login.ts
@@ -58,7 +58,7 @@ export class Login {
       });
     } else {
       this.loginForm.markAllAsTouched();
-      this.loginError = 'Credenciales no válidas';
+      this.loginError = 'Credenciales no válidas.';
     }
   }
 
