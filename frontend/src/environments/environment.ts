29a19761d402d251d738726e323392e257c293c4
María Carrera <marcartal1@alum.us.es>
2025-08-27 16:43:09 +0200

    fix: remove unnecesary userId constant

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   hostUrl:'http://localhost:8080/',
-  apiUrl: 'http://localhost:8080/api/v1/',
-  userId:1
+  apiUrl: 'http://localhost:8080/api/v1/'
 };
 
 /*
