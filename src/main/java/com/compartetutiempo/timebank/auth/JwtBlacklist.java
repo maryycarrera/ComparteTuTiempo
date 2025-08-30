2dced2491dfe7f4f847e402be8fda6e00b0eff22
María Carrera <marcartal1@alum.us.es>
2025-08-30 11:30:43 +0200

    feat: add token blacklist

---
+++ src/main/java/com/compartetutiempo/timebank/auth/JwtBlacklist.java
@@ -0,0 +1,19 @@
+package com.compartetutiempo.timebank.auth;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class JwtBlacklist {
+    private final Set<String> blacklist = ConcurrentHashMap.newKeySet();
+
+    public void add(String token) {
+        blacklist.add(token);
+    }
+
+    public boolean contains(String token) {
+        return blacklist.contains(token);
+    }
+}
