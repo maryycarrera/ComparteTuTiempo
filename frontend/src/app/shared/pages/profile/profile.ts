81d5348576779bcae5e121e7fb1c9b6995138b4d
María Carrera <marcartal1@alum.us.es>
2025-08-30 13:58:36 +0200

    feat: add shared profile

--- frontend/src/app/shared/pages/profile/profile.ts
+++ frontend/src/app/shared/pages/profile/profile.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AdminProfile } from '../../../admin/admin-profile/admin-profile';
+import { LoginService } from '../../../services/auth/login.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [AdminProfile],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
-export class Profile {
+export class Profile implements OnInit {
+
+  userIsAdmin: boolean = false;
+
+  private loginService = inject(LoginService);
+  private subscription: Subscription = new Subscription();
+
+  ngOnInit() {
+    this.subscription.add(this.loginService.userIsAdmin.subscribe({
+      next: (userAdminOn) => {
+        this.userIsAdmin = userAdminOn;
+      }
+    }));
+  }
 
 }
