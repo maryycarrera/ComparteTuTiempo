a190929889a28e982beede2ce6d7c6d52a350309
María Carrera <marcartal1@alum.us.es>
2025-08-30 12:13:07 +0200

    test: add logout tests

--- src/test/java/com/compartetutiempo/timebank/auth/AuthTest.java
+++ src/test/java/com/compartetutiempo/timebank/auth/AuthTest.java
@@ -5,6 +5,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import com.jayway.jsonpath.JsonPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,12 +17,17 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AuthTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    private static String memberToken;
+    private static String adminToken;
+
     @Test
+    @Order(1)
     public void shouldLoginMemberWhenCredentialsAreValid() throws Exception {
         String json = """
         {
@@ -27,17 +36,32 @@ public class AuthTest {
         }
         """;
 
-        mockMvc.perform(post("/api/v1/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").exists())
-                .andExpect(jsonPath("$.username").value("maryycarrera"))
-                .andExpect(jsonPath("$.authorities").isArray())
-                .andExpect(jsonPath("$.authorities[0]").value("MEMBER"));
+        String response = mockMvc.perform(post("/api/v1/auth/login")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(json))
+                                    .andExpect(status().isOk())
+                                    .andExpect(jsonPath("$.token").exists())
+                                    .andExpect(jsonPath("$.username").value("maryycarrera"))
+                                    .andExpect(jsonPath("$.authorities").isArray())
+                                    .andExpect(jsonPath("$.authorities[0]").value("MEMBER"))
+                                    .andReturn().getResponse().getContentAsString();
+
+        memberToken = JsonPath.read(response, "$.token");
     }
 
     @Test
+    @Order(2)
+    public void shouldLogoutMember() throws Exception {
+        String json = String.format("{\"token\": \"%s\"}", memberToken);
+
+        mockMvc.perform(post("/api/v1/auth/logout")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(3)
     public void shouldLoginAdminWhenCredentialsAreValid() throws Exception {
         String json = """
         {
@@ -46,14 +70,28 @@ public class AuthTest {
         }
         """;
 
-        mockMvc.perform(post("/api/v1/auth/login")
+        String response = mockMvc.perform(post("/api/v1/auth/login")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(json))
+                                    .andExpect(status().isOk())
+                                    .andExpect(jsonPath("$.token").exists())
+                                    .andExpect(jsonPath("$.username").value("admin1"))
+                                    .andExpect(jsonPath("$.authorities").isArray())
+                                    .andExpect(jsonPath("$.authorities[0]").value("ADMIN"))
+                                    .andReturn().getResponse().getContentAsString();
+
+        adminToken = JsonPath.read(response, "$.token");
+    }
+
+    @Test
+    @Order(4)
+    public void shouldLogoutAdmin() throws Exception {
+        String json = String.format("{\"token\": \"%s\"}", adminToken);
+
+        mockMvc.perform(post("/api/v1/auth/logout")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").exists())
-                .andExpect(jsonPath("$.username").value("admin1"))
-                .andExpect(jsonPath("$.authorities").isArray())
-                .andExpect(jsonPath("$.authorities[0]").value("ADMIN"));
+                .andExpect(status().isOk());
     }
 
     @Test
