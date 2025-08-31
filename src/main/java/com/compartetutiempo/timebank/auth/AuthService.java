b0a3188c2582a913760bb1d5df075ef0480dd8b4
María Carrera <marcartal1@alum.us.es>
2025-08-31 18:29:17 +0200

    feat: add register member to auth service

--- src/main/java/com/compartetutiempo/timebank/auth/AuthService.java
+++ src/main/java/com/compartetutiempo/timebank/auth/AuthService.java
@@ -1,12 +1,21 @@
 package com.compartetutiempo.timebank.auth;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.compartetutiempo.timebank.admin.AdministratorService;
+import com.compartetutiempo.timebank.auth.payload.request.SignupRequest;
+import com.compartetutiempo.timebank.member.Member;
 import com.compartetutiempo.timebank.member.MemberService;
+import com.compartetutiempo.timebank.user.Authority;
+import com.compartetutiempo.timebank.user.User;
 import com.compartetutiempo.timebank.user.UserService;
+
+import jakarta.validation.Valid;
 
 @Service
 public class AuthService {
@@ -24,30 +33,26 @@ public class AuthService {
         this.memberService = memberService;
     }
 
-    // public AuthResponse login(LoginRequest request) {
-    //     authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-    //     User user = userRepository.findByUsername(request.getUsername())
-    //             .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + request.getUsername()));
-    //     String token = jwtService.getToken(user);
-    //     return AuthResponse.builder()
-    //             .token(token)
-    //             .build();
-    // }
+    @Transactional
+    public void registerMember(@Valid SignupRequest signupRequest) {
+        User user = new User();
+        user.setUsername(signupRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        user.setAuthority(Authority.MEMBER);
 
-//     public AuthResponse register(RegisterRequest request) {
-//         User user = User.builder()
-//                 .name(request.getName())
-//                 .lastName(request.getLastName())
-//                 .username(request.getUsername())
-//                 .password(passwordEncoder.encode(request.getPassword()))
-//                 .email(request.getEmail())
-//                 .role(Authority.MEMBER)
-//                 .build();
+        Member member = new Member();
+        member.setName(signupRequest.getName());
+        member.setLastName(signupRequest.getLastName());
+        member.setEmail(signupRequest.getEmail());
+        member.setUser(user);
 
-//         userRepository.save(user);
+        member.setDateOfMembership(LocalDate.now());
+        member.setBiography("");
+        member.setHours(5);
+        member.setMinutes(0);
 
-//         return AuthResponse.builder()
-//                 .token(jwtService.getToken(user))
-//                 .build();
-//     }
+        memberService.save(member);
+    }
+
 }
+
