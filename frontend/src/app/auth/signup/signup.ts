19e3f7fd1bbfc2eada4d23a9949100ad0da3a6b1
María Carrera <marcartal1@alum.us.es>
2025-09-01 17:39:45 +0200

    feat: add signup component

---
+++ frontend/src/app/auth/signup/signup.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css'
+})
+export class Signup {
+
+}
