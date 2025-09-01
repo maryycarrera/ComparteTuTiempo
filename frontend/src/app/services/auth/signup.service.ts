f92a621e0e0c505828fdcda82aa3fa53ca13707e
María Carrera <marcartal1@alum.us.es>
2025-09-01 17:50:25 +0200

    feat: add logic to signup service

--- frontend/src/app/services/auth/signup.service.ts
+++ frontend/src/app/services/auth/signup.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { SignupRequest } from './payload/request/signup-request';
+import { Observable, throwError } from 'rxjs';
+import { MessageResponse } from './payload/response/message-response';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
-  
+
+  private http = inject(HttpClient);
+
+  signup(data: SignupRequest): Observable<MessageResponse> {
+    return this.http.post<MessageResponse>(environment.apiUrl + 'auth/login', data).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMsg = 'Error al registrar usuario.';
+    if (error.error && error.error.message) {
+      errorMsg = error.error.message;
+    } else if (error.error) {
+      errorMsg = error.error;
+    }
+    return throwError(() => new Error(errorMsg));
+  }
 }
