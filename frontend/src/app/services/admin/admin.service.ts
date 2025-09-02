7c26890f951b12f14222abd3907d39e37303388e
María Carrera <marcartal1@alum.us.es>
2025-09-02 19:27:40 +0200

    feat: add profile logic to admin service

--- frontend/src/app/services/admin/admin.service.ts
+++ frontend/src/app/services/admin/admin.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { AdminDTO } from './admin-dto';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-  
+
+  private http = inject(HttpClient);
+
+  getProfile(): Observable<AdminDTO> {
+    return this.http.get<AdminDTO>(environment.apiUrl + 'auth/profile').pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMsg = 'Error al obtener perfil de usuario.';
+    if (error.error && error.error.message) {
+      errorMsg = error.error.message;
+    } else if (error.error) {
+      errorMsg = error.error;
+    }
+    return throwError(() => new Error(errorMsg));
+  }
+
 }
