d7284ebe58e974d0df1cdcf0fa32a407de4dd15f
María Carrera <marcartal1@alum.us.es>
2025-09-03 18:26:59 +0200

    feat: personalize member service

--- frontend/src/app/services/member/member.service.ts
+++ frontend/src/app/services/member/member.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { LoginService } from '../auth/login.service';
+import { MemberProfile } from './member-profile';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
-  
+
+  private http = inject(HttpClient);
+  private loginService = inject(LoginService);
+
+  getProfile(): Observable<MemberProfile> {
+    const token = this.loginService.userToken;
+    return this.http.get<MemberProfile>(environment.apiUrl + 'auth/profile', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMsg = 'Error al obtener perfil de usuario.';
+    if (error.error && error.error.message) {
+      errorMsg = error.error.message;
+    } else if (error.error) {
+      errorMsg = error.error;
+    }
+    return throwError(() => new Error(errorMsg));
+  }
+
 }
