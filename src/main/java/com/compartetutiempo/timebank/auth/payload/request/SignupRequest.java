ceaff52b3d6cda470bb68a15649ad1f13adbd490
María Carrera <marcartal1@alum.us.es>
2025-09-08 18:07:17 +0200

    feat: add username pattern restriction

--- src/main/java/com/compartetutiempo/timebank/auth/payload/request/SignupRequest.java
+++ src/main/java/com/compartetutiempo/timebank/auth/payload/request/SignupRequest.java
@@ -2,6 +2,7 @@ package com.compartetutiempo.timebank.auth.payload.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,7 @@ public class SignupRequest {
 
     @NotEmpty
     @Size(min = 5, max = 15)
+    @Pattern(regexp = "^[a-z0-9_.]+$", message = "El nombre de usuario solo puede contener letras minúsculas (sin tildes, excluyendo 'ñ' y 'ç'), números, guiones bajos y puntos.")
     private String username;
 
     @NotEmpty
