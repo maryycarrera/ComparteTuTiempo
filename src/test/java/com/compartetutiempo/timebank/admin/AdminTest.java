06e8e1c35f2b2f39a4074de2f26d5d46e4fc6d13
María Carrera <marcartal1@alum.us.es>
2025-09-08 19:00:03 +0200

    test: add negative tests for create admin endpoint

--- src/test/java/com/compartetutiempo/timebank/admin/AdminTest.java
+++ src/test/java/com/compartetutiempo/timebank/admin/AdminTest.java
@@ -66,4 +66,157 @@ public class AdminTest {
                 .andExpect(jsonPath("$.object.profilePicture").value("/profilepics/gray.png"));
     }
 
+    @Test
+    public void shouldFailToCreateAdminWithDuplicateUsername() throws Exception {
+        String createJson = """
+        {
+            "name": "Taylor",
+            "lastName": "Swift",
+            "username": "admin1",
+            "email": "taylor.swift13@example.com",
+            "password": "il0vecats89!"
+        }
+        """;
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value("El dato 'Nombre de usuario' con valor 'admin1' ya está en uso."));
+    }
+
+    @Test
+    public void shouldFailToCreateAdminWithDuplicateEmail() throws Exception {
+        String createJson = """
+        {
+            "name": "Taylor",
+            "lastName": "Swift",
+            "username": "taylorswift13",
+            "email": "member1@example.com",
+            "password": "il0vecats89!"
+        }
+        """;
+
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value("El dato 'Dirección de correo electrónico' con valor 'member1@example.com' ya está en uso."));
+    }
+
+    @Test
+    public void shouldFailToCreateAdminWhenPasswordLacksSymbol() throws Exception {
+        String createJson = """
+        {
+            "name": "Taylor",
+            "lastName": "Swift",
+            "username": "taylorswift13",
+            "email": "taylor.swift.13@example.com",
+            "password": "il0vecats89"
+        }
+        """;
+
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldFailToCreateAdminWhenPasswordLacksNumber() throws Exception {
+        String createJson = """
+        {
+            "name": "Taylor",
+            "lastName": "Swift",
+            "username": "taylorswift13",
+            "email": "taylor.swift.13@example.com",
+            "password": "ilovecats!"
+        }
+        """;
+
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldFailToCreateAdminWhenPasswordLacksLowercase() throws Exception {
+        String createJson = """
+        {
+            "name": "Taylor",
+            "lastName": "Swift",
+            "username": "taylorswift13",
+            "email": "taylor.swift.13@example.com",
+            "password": "IL0VECATS89!"
+        }
+        """;
+
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldFailToCreateAdminWhenUsernameHasUppercase() throws Exception {
+        String createJson = """
+        {
+            "name": "Taylor",
+            "lastName": "Swift",
+            "username": "TaylorSwift13",
+            "email": "taylor.swift.13@example.com",
+            "password": "il0vecats89!"
+        }
+        """;
+
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldFailToCreateAdminWhenUsernameHasInvalidChars() throws Exception {
+        String createJson = """
+        {
+            "name": "Taylor",
+            "lastName": "Swift",
+            "username": "taylorswift-13!",
+            "email": "taylor.swift.13@example.com",
+            "password": "il0vecats89!"
+        }
+        """;
+
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldFailToCreateAdminWhenDataIsInvalid() throws Exception {
+        String signupJson = """
+        {
+            "name": "Al",
+            "lastName": "",
+            "username": "a",
+            "email": "invalid-email",
+            "password": "123"
+        }
+        """;
+
+        mockMvc.perform(post(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(signupJson))
+                .andExpect(status().isBadRequest());
+    }
+
 }
