7f81583345f2a4f65a6983a028142b9148a00e76
María Carrera <marcartal1@alum.us.es>
2025-09-09 17:55:14 +0200

    feat: add new admin button

--- frontend/src/app/admin/admin-list/admin-list.ts
+++ frontend/src/app/admin/admin-list/admin-list.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-list',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './admin-list.html',
   styleUrl: './admin-list.css'
 })
 export class AdminList {
+
+  private router = inject(Router);
+
+  create() {
+    this.router.navigate(['/administradores/crear']);
+  }
 
 }
