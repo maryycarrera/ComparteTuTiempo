2ec008deee90dac43972e3a22bcdc1ba8ac3aab8
María Carrera <marcartal1@alum.us.es>
2025-09-09 18:31:36 +0200

    feat: add base user form component

---
+++ frontend/src/app/components/base-user-form/base-user-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-user-form',
+  imports: [],
+  templateUrl: './base-user-form.html',
+  styleUrl: './base-user-form.css'
+})
+export class BaseUserForm {
+
+}
