830d34892758a7e39cbaec7614284c3e1ec6fc0c
María Carrera <marcartal1@alum.us.es>
2025-09-09 17:56:34 +0200

    feat: add create admin component

---
+++ frontend/src/app/admin/create-admin/create-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-admin',
+  imports: [],
+  templateUrl: './create-admin.html',
+  styleUrl: './create-admin.css'
+})
+export class CreateAdmin {
+
+}
