7a4eeaab3aec63c451bf5bb330db667f53183106
María Carrera <marcartal1@alum.us.es>
2025-09-09 19:02:42 +0200

    fix: add reusable base user form component to signup

--- frontend/src/app/admin/create-admin/create-admin.ts
+++ frontend/src/app/admin/create-admin/create-admin.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BaseUserForm } from '../../components/base-user-form/base-user-form';
 
 @Component({
   selector: 'app-create-admin',
-  imports: [ReactiveFormsModule, BaseUserForm],
+  imports: [BaseUserForm],
   templateUrl: './create-admin.html',
   styleUrl: './create-admin.css'
 })
