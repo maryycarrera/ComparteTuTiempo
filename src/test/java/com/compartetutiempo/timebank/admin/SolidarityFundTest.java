4b60b6a302091c512b9b885dd2d96ad0b2c657d4
María Carrera <marcartal1@alum.us.es>
2025-09-11 17:13:06 +0200

    test: add solidarity fund test for admin

---
+++ src/test/java/com/compartetutiempo/timebank/admin/SolidarityFundTest.java
@@ -0,0 +1,61 @@
+package com.compartetutiempo.timebank.admin;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.jayway.jsonpath.JsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SolidarityFundTest {
+
+    private static final String BASE_URL = "/api/v1/solidarity-fund";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private String adminToken;
+
+    @BeforeEach
+    void authenticateAdmin() throws Exception {
+        adminToken = getAdminToken();
+    }
+
+    private String getAdminToken() throws Exception {
+        String loginJson = """
+        {
+            "username": "admin2",
+            "password": "sys4dm1n*!"
+        }
+        """;
+
+        String loginResponse = mockMvc.perform(post("/api/v1/auth/login")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(loginJson))
+                                        .andReturn().getResponse().getContentAsString();
+
+        return JsonPath.read(loginResponse, "$.token");
+    }
+
+    @Test
+    public void shouldFindSolidarityFundSuccessfully() throws Exception {
+        mockMvc.perform(get(BASE_URL)
+                .header("Authorization", "Bearer " + adminToken)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Fondo Solidario encontrado con éxito."))
+                .andExpect(jsonPath("$.object.hours").value(100))
+                .andExpect(jsonPath("$.object.minutes").value(0));
+    }
+
+}
