979e52b20b18ce4eb967cc1fcccc9fc7dc87b3c2
María Carrera <marcartal1@alum.us.es>
2025-09-11 17:36:07 +0200

    feat: add admin solidarity fund component

---
+++ frontend/src/app/admin/admin-solidarity-fund/admin-solidarity-fund.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-solidarity-fund',
+  imports: [],
+  templateUrl: './admin-solidarity-fund.html',
+  styleUrl: './admin-solidarity-fund.css'
+})
+export class AdminSolidarityFund {
+
+}
