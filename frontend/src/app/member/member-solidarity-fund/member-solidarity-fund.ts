6d81994dedd219390bb0cc0270f220a4b3b8782c
María Carrera <marcartal1@alum.us.es>
2025-09-11 17:35:33 +0200

    feat: add member solidarity fund component

---
+++ frontend/src/app/member/member-solidarity-fund/member-solidarity-fund.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-solidarity-fund',
+  imports: [],
+  templateUrl: './member-solidarity-fund.html',
+  styleUrl: './member-solidarity-fund.css'
+})
+export class MemberSolidarityFund {
+
+}
