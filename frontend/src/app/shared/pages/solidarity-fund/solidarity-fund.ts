f8d17e9ce10223e34d989626ca4ad3fb1106f383
María Carrera <marcartal1@alum.us.es>
2025-09-11 17:37:30 +0200

    feat: personalize shared solidarity fund component

--- frontend/src/app/shared/pages/solidarity-fund/solidarity-fund.ts
+++ frontend/src/app/shared/pages/solidarity-fund/solidarity-fund.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from '../../../services/auth/login.service';
+import { Subscription } from 'rxjs';
+import { MemberSolidarityFund } from '../../../member/member-solidarity-fund/member-solidarity-fund';
+import { AdminSolidarityFund } from '../../../admin/admin-solidarity-fund/admin-solidarity-fund';
 
 @Component({
   selector: 'app-solidarity-fund',
-  imports: [],
+  imports: [MemberSolidarityFund, AdminSolidarityFund],
   templateUrl: './solidarity-fund.html',
   styleUrl: './solidarity-fund.css'
 })
-export class SolidarityFund {
+export class SolidarityFund implements OnInit, OnDestroy {
+
+  userIsAdmin: boolean = false;
+  
+    private loginService = inject(LoginService);
+    private subscription: Subscription = new Subscription();
+
+  ngOnInit() {
+    this.subscription.add(this.loginService.userIsAdmin.subscribe({
+      next: (userAdminOn) => {
+        this.userIsAdmin = userAdminOn;
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
