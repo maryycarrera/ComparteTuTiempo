2c4a2fcdfd281e5a88ede4c2099915d0bf89c1bb
María Carrera <marcartal1@alum.us.es>
2025-09-11 17:32:08 +0200

    feat: add shared solidarity fund component to frontend

---
+++ frontend/src/app/shared/pages/solidarity-fund/solidarity-fund.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solidarity-fund',
+  imports: [],
+  templateUrl: './solidarity-fund.html',
+  styleUrl: './solidarity-fund.css'
+})
+export class SolidarityFund {
+
+}
