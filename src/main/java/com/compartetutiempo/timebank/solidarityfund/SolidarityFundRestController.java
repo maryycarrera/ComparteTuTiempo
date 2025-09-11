377063db0f41c822475fa9451126d70c7e2be90f
María Carrera <marcartal1@alum.us.es>
2025-09-11 16:51:30 +0200

    feat: change message response class package

--- src/main/java/com/compartetutiempo/timebank/solidarityfund/SolidarityFundRestController.java
+++ src/main/java/com/compartetutiempo/timebank/solidarityfund/SolidarityFundRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.compartetutiempo.timebank.auth.payload.response.MessageResponse;
+import com.compartetutiempo.timebank.response.MessageResponse;
 
 @RestController
 @RequestMapping("/api/v1/solidarity-fund")
