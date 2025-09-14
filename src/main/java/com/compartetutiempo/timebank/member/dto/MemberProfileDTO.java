2cef3d7ed2b2955d85236ddecfab88f1e443b464
María Carrera <marcartal1@alum.us.es>
2025-09-14 19:56:22 +0200

    feat: add base member dto

--- src/main/java/com/compartetutiempo/timebank/member/dto/MemberProfileDTO.java
+++ src/main/java/com/compartetutiempo/timebank/member/dto/MemberProfileDTO.java
@@ -1,36 +1,18 @@
 package com.compartetutiempo.timebank.member.dto;
 
 import lombok.Getter;
-import java.time.format.DateTimeFormatter;
-
 import com.compartetutiempo.timebank.member.Member;
 
 @Getter
-public class MemberProfileDTO {
+public class MemberProfileDTO extends BaseMemberDTO {
 
     private String name;
     private String lastName;
-    private String username;
-    private String email;
-    private String profilePic;
-    private String biography;
-    private String dateOfMembership;
-    private String hours;
-    private String minutes;
 
     public MemberProfileDTO(Member member) {
+        super(member);
         this.name = member.getName();
         this.lastName = member.getLastName();
-        this.username = member.getUser().getUsername();
-        this.email = member.getEmail();
-        this.profilePic = member.getProfilePicture();
-        this.biography = member.getBiography();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        this.dateOfMembership = member.getDateOfMembership().format(formatter);
-
-        this.hours = member.getHours().toString();
-        this.minutes = member.getMinutes().toString();
     }
 
 }
