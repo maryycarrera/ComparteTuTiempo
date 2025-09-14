03c7fd6c8d5e24c8395486752e7ded3c8725c678
María Carrera <marcartal1@alum.us.es>
2025-09-14 18:43:31 +0200

    feat: filter members list qhen requesting user is member

--- src/main/java/com/compartetutiempo/timebank/member/MemberRestController.java
+++ src/main/java/com/compartetutiempo/timebank/member/MemberRestController.java
@@ -10,25 +10,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.compartetutiempo.timebank.payload.response.ListMessageResponse;
+import com.compartetutiempo.timebank.user.Authority;
+import com.compartetutiempo.timebank.user.User;
+import com.compartetutiempo.timebank.user.UserService;
 
 @RestController
 @RequestMapping("/api/v1/members")
 public class MemberRestController {
 
     private final MemberService memberService;
+    private final UserService userService;
 
     @Autowired
-    public MemberRestController(MemberService memberService) {
+    public MemberRestController(MemberService memberService, UserService userService) {
         this.memberService = memberService;
+        this.userService = userService;
     }
 
     @GetMapping
     public ResponseEntity<ListMessageResponse<MemberListDTO>> findAllMembers() {
+        User currentUser = userService.findCurrentUser();
+        Authority authority = currentUser.getAuthority();
+
         List<MemberListDTO> members = memberService.findAll();
+
+        if (authority.equals(Authority.MEMBER)) {
+            members = members.stream()
+                    .filter(member -> !member.getUsername().equals(currentUser.getUsername()))
+                    .toList();
+        }
+
         if (members.isEmpty()) {
             ListMessageResponse<MemberListDTO> response = new ListMessageResponse<MemberListDTO>("¡Vaya! Parece que no hay miembros registrados aún.");
             return ResponseEntity.ok(response);
         }
+
         ListMessageResponse<MemberListDTO> response = new ListMessageResponse<MemberListDTO>("Lista de miembros encontrada con éxito.", members);
         return ResponseEntity.ok(response);
     }
