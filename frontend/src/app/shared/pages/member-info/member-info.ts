620e6ccd8cdbc0b7f1498a29b953b977b3adad68
María Carrera <marcartal1@alum.us.es>
2025-09-15 17:51:10 +0200

    feat: personalize member info component

--- frontend/src/app/shared/pages/member-info/member-info.ts
+++ frontend/src/app/shared/pages/member-info/member-info.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { MemberService } from '../../../services/member/member.service';
+import { MemberDTO } from '../../../services/member/dto/member-dto';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ResourcesService } from '../../../services/resources/resources.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-member-info',
@@ -6,6 +12,55 @@ import { Component } from '@angular/core';
   templateUrl: './member-info.html',
   styleUrl: './member-info.css'
 })
-export class MemberInfo {
+export class MemberInfo implements OnInit, OnDestroy {
+
+  private memberService = inject(MemberService);
+  private resourcesService = inject(ResourcesService);
+  private activatedRoute = inject(ActivatedRoute);
+  private subscription: Subscription = new Subscription();
+
+  static readonly DEFAULT_PROFILE_PICTURE: string = environment.hostUrl + 'profilepics/black.png';
+
+  memberId!: string;
+  member?: MemberDTO;
+  errorMessage?: string;
+  profilePicture: string = MemberInfo.DEFAULT_PROFILE_PICTURE;
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.activatedRoute.params.subscribe(params => {
+        this.memberId = params['id'];
+        this.memberService.getMemberById(this.memberId).subscribe({
+          next: (response) => {
+            this.member = response.object;
+
+            let picUrl = this.profilePicture;
+            if (response.object?.profilePic && response.object.profilePic !== '') {
+              picUrl = response.object.profilePic;
+              if (!picUrl.startsWith('http')) {
+                picUrl = environment.hostUrl + response.object.profilePic;
+              }
+            }
+
+            this.resourcesService.getProfilePicture(picUrl).subscribe({
+              next: (blob) => {
+                this.profilePicture = URL.createObjectURL(blob);
+              },
+              error: (err) => {
+                this.errorMessage = err && err.message ? err.message : String(err);
+              }
+            });
+          },
+          error: (error) => {
+            this.errorMessage = error && error.message ? error.message : String(error);
+          }
+        });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
