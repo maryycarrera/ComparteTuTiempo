7990dfe0fe09b1f80cb7612fbd22c848f4544d0d
María Carrera <marcartal1@alum.us.es>
2025-09-15 18:14:21 +0200

    feat: add details button component

---
+++ frontend/src/app/components/details-button/details-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-button',
+  imports: [],
+  templateUrl: './details-button.html',
+  styleUrl: './details-button.css'
+})
+export class DetailsButton {
+
+}
