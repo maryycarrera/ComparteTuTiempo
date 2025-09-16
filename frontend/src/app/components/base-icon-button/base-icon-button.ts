623776d58dbdc87bae5495032f8b6468df6fe61e
María Carrera <marcartal1@alum.us.es>
2025-09-16 19:00:54 +0200

    feat: personalize base bttn

--- frontend/src/app/components/base-icon-button/base-icon-button.ts
+++ frontend/src/app/components/base-icon-button/base-icon-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-base-icon-button',
@@ -7,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrl: './base-icon-button.css'
 })
 export class BaseIconButton {
+
+  @Input() bttnType: 'button' | 'delete' | 'details' = 'button';
+
+  @Output() clicked = new EventEmitter<void>();
+
+  getIcon(): string {
+    switch (this.bttnType) {
+      case 'delete':
+        return 'bi-trash-fill';
+      case 'details':
+        return 'bi-info-circle-fill';
+      case 'button':
+      default:
+        return 'bi-question-circle-fill';
+    }
+  }
+
+  getTitle(): string {
+    switch (this.bttnType) {
+      case 'delete':
+        return 'Eliminar';
+      case 'details':
+        return 'Ver detalles';
+      case 'button':
+      default:
+        return 'Botón';
+    }
+  }
+
+  getClass(): string {
+    switch (this.bttnType) {
+      case 'delete':
+        return 'btn btn-danger btn-sm';
+      case 'details':
+        return 'btn btn-info btn-sm';
+      case 'button':
+      default:
+        return 'btn btn-secondary btn-sm';
+    }
+  }
+
+  onClick() {
+    this.clicked.emit();
+  }
 
 }
