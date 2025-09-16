ac8e60ecbcc244f2ed23763e34a6172af2ef4d83
María Carrera <marcartal1@alum.us.es>
2025-09-16 18:38:25 +0200

    fix: isolate delete member admin test

--- src/test/java/com/compartetutiempo/timebank/admin/MemberTest.java
+++ src/test/java/com/compartetutiempo/timebank/admin/MemberTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.compartetutiempo.timebank.BaseTest;
 
@@ -76,6 +77,7 @@ public class MemberTest extends BaseTest {
     }
 
     @Test
+    @Transactional
     public void shouldDeleteMemberSuccessfully() throws Exception {
         int memberIdToDelete = 11;
 
