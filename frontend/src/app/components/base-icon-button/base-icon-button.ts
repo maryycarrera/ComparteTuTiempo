a069a2e3181bc0d88e6a1f0ab362b3a6c065f613
María Carrera <marcartal1@alum.us.es>
2025-09-16 18:53:03 +0200

    feat: add base icon button component

---
+++ frontend/src/app/components/base-icon-button/base-icon-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-icon-button',
+  imports: [],
+  templateUrl: './base-icon-button.html',
+  styleUrl: './base-icon-button.css'
+})
+export class BaseIconButton {
+
+}
