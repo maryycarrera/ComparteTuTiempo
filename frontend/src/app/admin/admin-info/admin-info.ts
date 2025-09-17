6ef8030d7e16d7afe7fa557a247ba1b8ffcd6f14
María Carrera <marcartal1@alum.us.es>
2025-09-17 20:46:34 +0200

    feat: create admin info component

---
+++ frontend/src/app/admin/admin-info/admin-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-info',
+  imports: [],
+  templateUrl: './admin-info.html',
+  styleUrl: './admin-info.css'
+})
+export class AdminInfo {
+
+}
