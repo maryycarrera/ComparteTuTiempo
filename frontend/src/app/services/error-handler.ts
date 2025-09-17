0953b05a9d8902c7d87df90da1a39c99f8eab9cf
María Carrera <marcartal1@alum.us.es>
2025-09-17 18:35:44 +0200

    refactor: fix indentation

--- frontend/src/app/services/error-handler.ts
+++ frontend/src/app/services/error-handler.ts
@@ -5,9 +5,9 @@ export class ErrorHandler {
     public handleError(error: HttpErrorResponse, defaultMessage: string) {
         let errorMsg = defaultMessage;
         if (error.error && error.error.message) {
-        errorMsg = error.error.message;
+            errorMsg = error.error.message;
         } else if (error.error) {
-        errorMsg = error.error;
+            errorMsg = error.error;
         }
         return throwError(() => new Error(errorMsg));
     }
