856988d396d629735f4293ae6ceb66f0b4634574
María Carrera <marcartal1@alum.us.es>
2025-09-18 19:38:34 +0200

    fix: update invalid profile pic exception message

--- src/main/java/com/compartetutiempo/timebank/exceptions/InvalidProfilePictureException.java
+++ src/main/java/com/compartetutiempo/timebank/exceptions/InvalidProfilePictureException.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public class InvalidProfilePictureException extends RuntimeException {
 
     public InvalidProfilePictureException(String color, String action) {
-        super(String.format("The color %s is not a valid profile picture color for action: '%s'", color, action));
+        super(String.format("The color '%s' is not a valid profile picture color for action: '%s'", color, action));
     }
 
     public InvalidProfilePictureException() {
